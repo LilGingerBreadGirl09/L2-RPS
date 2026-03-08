1a0e16d7276d2ef64ab1b5e526b864381a713a20
LilGingerBreadGirl09 <gemotop24944@masseyhigh.school.nz>
2026-03-09 12:13:46 +1300

    Working loop, needs headings

---
+++ B_01_rps_game_v1.py
@@ -0,0 +1,69 @@
+# Check that users have entered a valid
+# option based on a list
+def int_check(question):
+    """Checks users enter an integer more than / equal to 13"""
+
+    error = "Please enter an integer more than / equal to 1. >:["
+
+    while True:
+
+        to_check = input(question)
+
+        # check for infinite mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+
+            # checks that the number is more than / equal to 1
+            if response < 1:
+                print(error)
+
+            else:
+               return response
+
+        except ValueError:
+            print(error)
+
+
+
+
+# Main routine starts here
+
+# Intialize game variables
+mode = "regular"
+rounds_played = 0
+
+print("🪨📃✂️ Rock / Paper / Scissors Game ✂️📃🪨")
+print()
+
+# Instructions
+
+#Ask user for number of rounds / infinite mode
+num_rounds = int_check("How many rounds would you like to play? Push <enter> for infinite mode: ")
+
+if num_rounds == "infinite":
+    mode = "infinite"
+    print("You chose infinite")
+    num_rounds = 5
+
+#Game loop starts here
+while rounds_played < num_rounds:
+    user_choice = input("Choose: ")
+
+    if user_choice == "xxx":
+        break
+
+    rounds_played += 1
+    print("rounds played: ", rounds_played)
+
+    # If users are in infinite mode, increase number of rounds!
+    if mode == "infinite":
+        num_rounds += 1
+
+    print("num rounds: ", num_rounds)
+
+#Game loop ends here
+
+#Game history / Statistics here
